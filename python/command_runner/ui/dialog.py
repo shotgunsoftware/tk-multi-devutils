b6ec49a208befb1401c96069ab57c3b178f25bdf
Eduardo Chauca <eduardo.chauca@autodesk.com>
2024-09-05 15:02:43 -0500

    new import

--- python/command_runner/ui/dialog.py
+++ python/command_runner/ui/dialog.py
@@ -8,11 +8,11 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from tank.platform.qt import QtCore
+from . import QtCore
 for name, cls in QtCore.__dict__.items():
     if isinstance(cls, type): globals()[name] = cls
 
-from tank.platform.qt import QtGui
+from . import QtGui
 for name, cls in QtGui.__dict__.items():
     if isinstance(cls, type): globals()[name] = cls
 
